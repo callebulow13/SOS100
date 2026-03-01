134e9080e3daaa79846faa16715ec81ecf213917
Calle Bülow <cabu0004@student.hv.se>
2026-03-01 15:24:48 +0100

    Admin can create, update and delete users

--- UserService/Controllers/UserController.cs
+++ UserService/Controllers/UserController.cs
@@ -1,9 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using UserService.Data;
 using UserService.Dtos;
 using UserService.Models;
-
 namespace UserService.Controllers;
 
 [ApiController]
@@ -34,7 +34,6 @@ public class UserController : ControllerBase
             user.Role
         });
     }
-    
     [HttpGet]
     public User[] GetUsers()
     {
@@ -42,10 +41,57 @@ public class UserController : ControllerBase
         return users;
     }
 
+    [HttpGet("{id}")]
+    public ActionResult<User> GetUser(int id)
+    {
+        var user = _dbContext.Users.Find(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        return Ok(user);
+    }
+    
+
     [HttpPost]
     public void AddUser(User user)
     {
         _dbContext.Users.Add(user);
         _dbContext.SaveChanges();
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateUser(int id, User updatedUser)
+    {
+        var user = _dbContext.Users.Find(id);
+        if (user == null)
+            return NotFound();
+        
+        user.Username = updatedUser.Username;
+        user.Email = updatedUser.Email;
+        user.FirstName = updatedUser.FirstName;
+        user.LastName = updatedUser.LastName;
+        user.Role = updatedUser.Role;
+
+        _dbContext.SaveChanges();
+        
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteUser(int id)
+    {
+        var user = _dbContext.Users.Find(id);
+
+        if (user == null)
+            return NotFound();
+        if (user.Username == "admin")
+        {
+            return BadRequest("Kan inte radera admin");
+        }
+        
+        _dbContext.Users.Remove(user);
+        _dbContext.SaveChanges();
+        return NoContent();
     }
 }

4f5d0567516b9ca89d2160963701aa202792d15d
mapo0020 <mapo0020@student.hv.se>
2026-03-04 09:45:18 +0100

    ReminderApi klar med CRUD och databas

---
+++ ReminderApi/Models/Reminder.cs
@@ -0,0 +1,17 @@
+﻿namespace ReminderApi.Models;
+
+public class Reminder
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int LoanId { get; set; }
+    public int ItemId { get; set; }
+    public string ItemTitle { get; set; } = string.Empty;
+    public DateTime DueDate { get; set; }
+    public bool IsSent { get; set; } = false;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public int? WatchId { get; set; }
+    public Watch? Watch { get; set; }
+ 
+}

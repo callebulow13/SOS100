ff4dfe3106b48c357aa32443a87c3813788d67d7
mapo0020 <mapo0020@student.hv.se>
2026-03-16 21:10:37 +0100

    database update

--- ReminderApi/Controllers/ReminderController.cs
+++ ReminderApi/Controllers/ReminderController.cs
@@ -5,7 +5,7 @@ using ReminderApi.Models;
 using ReminderApi.Filters;
 
 namespace ReminderApi.Controllers;
-// GET
+// 
 [ApiController]
 [Route("api/[controller]")]
 [ServiceFilter(typeof(ApiKeyFilter))]
